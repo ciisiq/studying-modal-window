ddbf9d58ffd37b51f79d9a7f861ab1a46c3447c0
ciisiq <cii_3@hotmail.com>
2022-04-08 14:53:01 +0100

    start basic

---
+++ script.js
@@ -0,0 +1,10 @@
+'use strict';
+// How to work with class
+
+const modal = document.querySelector('modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  console.log(btnsOpenModal[i].textContent);
