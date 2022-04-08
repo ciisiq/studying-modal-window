ca91882b11b21a98d4845f720fd4dbb20ff72aa7
ciisiq <cii_3@hotmail.com>
2022-04-08 17:06:32 +0100

    80 working with classes

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 'use strict';
 // How to work with class
-
-const modal = document.querySelector('modal');
+const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+//function to open the modal
+const openModal = function () {
+  console.log('Button clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+//function to close the modal
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++)
-  console.log(btnsOpenModal[i].textContent);
+  //show what was hidden
+  btnsOpenModal[i].addEventListener('click', openModal);
+//creating button to close window
+btnCloseModal.addEventListener('click', closeModal);
+//creating option to close everywhere
+overlay.addEventListener('click', closeModal);
